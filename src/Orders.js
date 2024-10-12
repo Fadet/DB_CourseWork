2c6c3e1ab2c1394e2ec43f71bf07981451a12dad
Lolik232 <vlad.vavilin73@gmail.com>
2024-10-12 17:08:47 +0300

    Added order card

--- src/Orders.js
+++ src/Orders.js
@@ -1,35 +1,45 @@
 import React, {useContext, useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './Order.css';
 import {useAuthUser} from "react-auth-kit";
 import {UserOrdersDataContext} from "./MainComponent";
-import data from "bootstrap/js/src/dom/data";
+
+function formatDate(date) {
+  return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+}
+
 
 export function Orders() {
-    const userData = useAuthUser();
-    const [userOrdersData, setUserOrdersData] = useContext(UserOrdersDataContext);
-    const [orders, setOrders] = useState(undefined);
+  const userData = useAuthUser();
+  const [userOrdersData, setUserOrdersData] = useContext(UserOrdersDataContext);
+  const [orders, setOrders] = useState(undefined);
 
-    useEffect(() => {
-        const index = userOrdersData.findIndex((obj) => obj.id === userData().email);
-        if (index !== -1) {
-            setOrders(userOrdersData[index]);
-        }
-    }, []);
+  useEffect(() => {
+    // setOrders({data: ORDERS});
+    const index = userOrdersData.findIndex((obj) => obj.id === userData().email);
+    if (index !== -1) {
+      setOrders(userOrdersData[index]);
+    }
+  }, []);
 
-    return (
-        <>
-            {(orders !== undefined) && orders.data.map((order)=>(
-                <div>
-                    <h1>Заказ №{order.id}</h1>
-                    <div>
-                        <h2>Дата покупки: {order.date}</h2>
-                        <h3>Сумма заказа: {order.price}</h3>
-                    </div>
-                    <div>
-                        Статус: {order.status}
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+  return (
+    (orders !== undefined) && orders.data.map((order) =>
+      <Order
+        key={order.id}
+        {...order}
+      />
+    )
+  );
+}
+
+function Order({id, date, price, status}) {
+  return <>
+    <div className={'order_card d-flex flex-row align-items-center justify-content-between'}>
+      <h3>Заказ №{id}</h3>
+      <h4 className={'text-secondary opacity-75'}>{formatDate(new Date(date))}</h4>
+      <h4>{status}</h4>
+      <h4>{price}</h4>
+    </div>
+  </>
 }
